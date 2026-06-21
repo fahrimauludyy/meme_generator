b162478b8f9b939c603f5871dd5062e78434aa96
Fahri Mauludy <idnmuhammadfahri2008@gmail.com>
2026-06-21 19:48:16 +0700

    Adding API

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import memeImage from "../assets/memeImage.png"
 
 export default function Home(){
@@ -9,6 +9,14 @@ export default function Home(){
             imgUrl: memeImage
         }
     )
+
+    const [allMemes, setAllMemes] = useState([])
+
+    useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+        .then(res => res.json())
+        .then(data => setAllMemes(data.data.memes))
+    },[] )
 
     function handleChange(e) {
         const {value, name} = e.currentTarget
